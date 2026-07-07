478bcded4b0e1ddca4a5adb25c1cdf89a6d808a4
MilosN05 <ninkovicmilos148@gmail.com>
2026-07-08 01:40:53 +0200

    Add: same-binary-tree - submission-1

---
+++ Data Structures & Algorithms/same-binary-tree/submission-1.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+
+    def max_depth(self,p:TreeNode):
+        if not p:return 0
+
+
+        return 1+max(self.max_depth(p.left),self.max_depth(p.right))
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        solucija = Solution()
+        visina_p = solucija.max_depth(p)
+        visina_q = solucija.max_depth(q)
+
+        if visina_p!=visina_q: return False
+
+        return self.isSameTree_helper(p,q)
+
+
+    def isSameTree_helper(self, p: TreeNode, q: TreeNode) -> bool:
+
+        if (not p) and (not q): return True
+        if (p and not q) or (not p and q):return False
+        if p.val==q.val:
+            return self.isSameTree_helper(p.left,q.left) and self.isSameTree_helper(p.right,q.right)
+        return False
