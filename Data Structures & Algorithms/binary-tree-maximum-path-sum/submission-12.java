69a7fe1330f1dab47940d3c9c777d10a2acefa7c
MilosN05 <ninkovicmilos148@gmail.com>
2026-07-24 11:21:55 +0200

    Add: binary-tree-maximum-path-sum - submission-12

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-12.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+
+    int max_suma_grana = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        if (root ==null) return Integer.MIN_VALUE;
+        
+        
+
+        return Math.max(max_path_sum_helper(root),max_suma_grana);
+    }
+
+    private int max_path_sum_helper(TreeNode root) {
+        if (root==null) return Integer.MIN_VALUE;
+        if (root.left==null && root.right==null) return root.val;
+
+        int levo_ps = max_path_sum_helper(root.left);
+        int desno_ps = max_path_sum_helper(root.right);
+
+        
+        if (levo_ps>0 && desno_ps>0 && (root.val + levo_ps + desno_ps > max_suma_grana) )
+            max_suma_grana = root.val + levo_ps + desno_ps;
+
+        if (levo_ps>max_suma_grana)
+            max_suma_grana = levo_ps;
+        
+        if (desno_ps>max_suma_grana)
+            max_suma_grana = desno_ps;
+        
+        if ((levo_ps >= desno_ps) && levo_ps > 0 )
+            return levo_ps + root.val;
+        else if ((desno_ps > levo_ps) && desno_ps>0 )
+            return desno_ps + root.val;
+        return root.val;
+    }
+}
