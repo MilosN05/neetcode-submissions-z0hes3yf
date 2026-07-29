2d5c3954ce77f75655b6c5d56b4a4cab498ef86f
MilosN05 <ninkovicmilos148@gmail.com>
2026-07-29 11:47:56 +0200

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    List<List<Integer>> required_subsets;
+    public List<List<Integer>> subsets(int[] nums) {
+        if (nums==null) return null;
+
+        required_subsets = new LinkedList<>();
+        backtrack(nums,new LinkedList<>(), 0);
+        return required_subsets;
+    }
+
+    public void backtrack(int[] nums, LinkedList<Integer> subset,int index) {
+
+        if (nums.length==index) {
+            required_subsets.add(new LinkedList<>(subset));
+
+            return;
+        }
+
+        subset.add(nums[index]);
+        backtrack(nums,subset,index+1);
+
+        subset.remove(subset.getLast());
+        backtrack(nums,subset,index+1);
+
+    }
+}
