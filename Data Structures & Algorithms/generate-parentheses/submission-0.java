6b0bd0c87e544483b8b68667a1e50fdd44a5b769
MilosN05 <ninkovicmilos148@gmail.com>
2026-08-05 11:45:01 +0200

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.java
@@ -0,0 +1,54 @@
+class Solution {
+    List<String> result;
+    public List<String> generateParenthesis(int n) {
+        if (n<=0) return null;
+
+        result = new ArrayList<>();
+        backtrack(n, 0,0, new StringBuilder());
+
+        return result;
+    }
+
+    private void backtrack(int n, int num_op,int num_cl, StringBuilder cur_string) {
+        System.out.println(cur_string + " num_op: " + num_op + " num_cl: " +num_cl);
+        if ((num_op ==n) && (num_cl==n)) {
+            result.add(cur_string.toString());
+            return;
+        }
+
+
+        for (int i =0;i<2;i++) {
+            
+
+            if (i>0 && num_op==0) 
+                continue;
+            else if (num_op==0 ) {
+                cur_string.append('(');
+                backtrack(n, num_op+1, num_cl, cur_string);  
+                cur_string.deleteCharAt(cur_string.length()-1);
+
+            } 
+            else if (i==0 && num_op<n) {
+                cur_string.append('(');
+                backtrack(n, num_op+1, num_cl, cur_string);  
+                cur_string.deleteCharAt(cur_string.length()-1);
+
+
+            }
+            else if (i==1 && num_op>num_cl){
+                cur_string.append(')');
+                backtrack(n, num_op, num_cl+1, cur_string);  
+                cur_string.deleteCharAt(cur_string.length()-1);
+
+            }
+
+
+        }
+
+        
+
+
+             
+
+    }
+}
