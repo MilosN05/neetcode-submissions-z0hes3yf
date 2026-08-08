e4dfa9a93c10e8b3fe7379e4e08fb626787411bf
MilosN05 <ninkovicmilos148@gmail.com>
2026-08-09 00:21:24 +0200

    Add: search-for-word - submission-15

---
+++ Data Structures & Algorithms/search-for-word/submission-15.java
@@ -0,0 +1,64 @@
+class Solution {
+    // HashSet<String> pos_grid;
+    boolean[][] pos_grid;
+    public boolean exist(char[][] board, String word) {
+        if (word==null || board==null) return false;
+
+        pos_grid = new boolean[board.length][board[0].length];
+        return backtrack(board, word, 0,0, 0, true);
+    }
+
+    private boolean backtrack(char[][] board, String word, int i, int j, int index, boolean pocetak) {
+  
+
+        if (i>=board.length || j>=board[0].length || i<0 || j< 0|| (pos_grid[i][j] ))
+            return false;
+        
+
+        if (pocetak==true) {}
+        else if (board[i][j]==word.charAt(index)) {
+            pos_grid[i][j]=true;
+            index+=1;
+        }
+        else 
+            return false;
+
+        if (index==word.length())
+            return true;
+        
+    
+        if (index==0) {
+            for (int v=0;v<board.length;v++)
+                for (int k=0;k<board[0].length;k++) {
+                    if (board[v][k]==word.charAt(0)) {
+                        boolean resenje = backtrack(board, word, v,k, index, false);
+                        pos_grid[v][k]=false;
+                        if (resenje==true)
+                            return true;
+                        }
+                }
+            }
+
+        else {
+            
+            boolean levo = backtrack(board,word,i,j-1,index, false);
+            if (levo)
+                return true;
+            boolean desno = backtrack(board,word,i,j+1,index, false);
+            if (desno)
+                return true;
+            boolean gore = backtrack(board,word,i-1,j,index, false);
+            if (gore)
+                return true;
+            boolean dole = backtrack(board,word,i+1,j,index, false);
+            if (dole)
+                return true;
+       
+
+            ///pos_grid.remove je problem to popravljaj, pogeldaj sta ti gemini pise
+            
+        }
+        pos_grid[i][j]=false;
+        return false;
+    }
+}
